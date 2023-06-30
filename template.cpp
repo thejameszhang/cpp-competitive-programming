d4202259727c8887231e39b2ff74ea124be45d0e
James Zhang 张衣涵 <70459407+thejameszhang@users.noreply.github.com>
2023-06-30 10:10:19 -0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+class Solution {
+  public:
+    int solution() {
+      // solution comes here
+    }
+  private:
+    void helper_function() {
+      // any necessary helpers should be private
+    }
+}
+
+int main() {
+	// testing
+}
